ccc305ba13f511e2ed4fa3a0ee009d4727da77d5
Marcos Galdamez <theblueflash@Mac.lan>
2026-01-13 15:17:05 -0500

    Initialized my first few lines of code for this class project

---
+++ src/NavBar.tsx
@@ -0,0 +1,19 @@
+import './NavBar.css'
+
+function NavBar(){
+
+
+    return (
+        <>
+        <nav>
+            <div>
+                <button><a href="">Home</a></button>
+                <button><a href="">About</a></button>
+                <button><a href="">Experience</a></button>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default NavBar;
