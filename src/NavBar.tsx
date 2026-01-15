34a6144b0699e8d41cebae25b2ac5c35283d7250
Marcos Galdamez <theblueflash@eduroam-226-5-210.laptops.usf.edu>
2026-01-15 10:28:55 -0500

    Added a new styling sheet

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,35 +1,36 @@
 
-function NavBar(){
+function NavBar() {
 
     const navbarStyle: React.CSSProperties = {
-            backgroundColor: 'darkblue',
-            display: "flex",
-            width: 350,
-            height: 50,
-            border: '3px solid',
-            borderColor: "white",
-            borderRadius: 6,
-            justifyContent: "center",
-            alignItems: "center"
+        backgroundColor: 'darkblue',
+        display: "flex",
+        width: 350,
+        height: 50,
+        border: '3px solid',
+        borderColor: "white",
+        borderRadius: 6,
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "0 auto"
     };
 
     const navbarButtonsStyle: React.CSSProperties = {
         marginRight: 2,
         marginLeft: 2,
         borderBottomLeftRadius: 25,
-        borderBottomRightRadius: 25 
+        borderBottomRightRadius: 25
     };
 
 
     return (
         <>
-        <nav style={navbarStyle}>
-            <div>
-                <button style={navbarButtonsStyle}><a href="">Home</a></button>
-                <button style={navbarButtonsStyle}><a href="">About</a></button>
-                <button style={navbarButtonsStyle}><a href="">Experience</a></button>
-            </div>
-        </nav>
+            <nav style={navbarStyle}>
+                <div>
+                    <button style={navbarButtonsStyle}><a href="">Home</a></button>
+                    <button style={navbarButtonsStyle}><a href="">About</a></button>
+                    <button style={navbarButtonsStyle}><a href="">Experience</a></button>
+                </div>
+            </nav>
         </>
     )
 }
